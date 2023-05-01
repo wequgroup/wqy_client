12d6daf74cd07a9a159b9587773b8ab964941d68
lrqtech <admin@lrqtech.net>
2023-05-01 19:08:37 -0400

    Update create_dmg.sh

--- create_dmg.sh
+++ create_dmg.sh
@@ -7,13 +7,13 @@ fi
 
 file_path="$1"
 volume_name="app"
-dmg_size="80M"
+dmg_size="100M"
 
 hdiutil create -size $dmg_size -fs HFS+ -volname $volume_name "${volume_name}.dmg"
 mount_info=$(hdiutil attach "${volume_name}.dmg")
 device_path=$(echo "$mount_info" | grep -o "/dev/disk[0-9]*s[0-9]*")
 mount_point=$(echo "$mount_info" | grep -o "/Volumes/$volume_name")
-cp "$file_path" "$mount_point/$(basename "$file_path")"
+cp -R "$file_path" "$mount_point/"
 hdiutil detach "$device_path"
 hdiutil convert "${volume_name}.dmg" -format UDZO -o "compressed_${volume_name}.dmg"
 rm "${volume_name}.dmg"
